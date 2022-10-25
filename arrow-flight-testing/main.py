543ff4df7601aa4919640764f6cad764ea2c3eac
Søren Kejser Jensen <devel@kejserjensen.dk>
2022-10-25 17:52:48 +0200

    Reorder the PyArrow methods to match FlightService

--- arrow-flight-testing/main.py
+++ arrow-flight-testing/main.py
@@ -6,46 +6,17 @@ import pandas as pd
 from pyarrow import flight
 
 
-def create_table_action(client, table_name):
-    action_body = str.encode(
-        "CREATE TABLE " + table_name +
-        "(timestamp TIMESTAMP, values REAL, metadata REAL)")
-
-    action = pyarrow.flight.Action("CommandStatementUpdate", action_body)
-    result = client.do_action(action)
-
-    print(list(result))
-
-
-def create_model_table_action(client, table_name):
-    action_body = str.encode(
-        "CREATE MODEL TABLE " + table_name + "(location TAG, install_year TAG, model TAG, timestamp TIMESTAMP, power_output FIELD, wind_speed FIELD, temperature FIELD(5))")
-
-    action = pyarrow.flight.Action("CommandStatementUpdate", action_body)
-    result = client.do_action(action)
-
-    print(list(result))
-
-
-def list_actions(client):
-    result = client.list_actions()
-    print(list(result))
-
-
-def do_put(client, table_name):
-    upload_descriptor = pyarrow.flight.FlightDescriptor.for_path(table_name)
-    writer, _ = client.do_put(upload_descriptor, get_pyarrow_schema())
-
-    record_batch = create_record_batch(10000)
-    writer.write(record_batch)
-
-
-def do_get(flight_client, table_name):
-    ticket = flight.Ticket("SELECT * FROM " + table_name + " LIMIT 5")
-    response = flight_client.do_get(ticket)
-
-    for batch in response:
-        print(batch)
+# Helper Functions.
+def get_pyarrow_schema():
+    return pyarrow.schema([
+        ("location", pyarrow.utf8()),
+        ("install_year", pyarrow.utf8()),
+        ("model", pyarrow.utf8()),
+        ("timestamp", pyarrow.timestamp("ms")),
+        ("power_output", pyarrow.float32()),
+        ("wind_speed", pyarrow.float32()),
+        ("temperature", pyarrow.float32()),
+    ])
 
 
 def create_record_batch(num_rows):
@@ -64,37 +35,57 @@ def create_record_batch(num_rows):
     return pyarrow.RecordBatch.from_pandas(df=df, schema=get_pyarrow_schema())
 
 
+# PyArrow Function.
 def list_flights(flight_client):
     response = flight_client.list_flights()
 
     print(response)
 
 
-def get_schema(client, table_name):
+def get_schema(flight_client, table_name):
     upload_descriptor = pyarrow.flight.FlightDescriptor.for_path(table_name)
-    response = client.get_schema(upload_descriptor)
+    response = flight_client.get_schema(upload_descriptor)
 
     print(response.schema)
 
 
-def get_pyarrow_schema():
-    return pyarrow.schema([
-        ("location", pyarrow.utf8()),
-        ("install_year", pyarrow.utf8()),
-        ("model", pyarrow.utf8()),
-        ("timestamp", pyarrow.timestamp("ms")),
-        ("power_output", pyarrow.float32()),
-        ("wind_speed", pyarrow.float32()),
-        ("temperature", pyarrow.float32()),
-    ])
+def do_get(flight_client, table_name):
+    ticket = flight.Ticket("SELECT * FROM " + table_name + " LIMIT 5")
+    response = flight_client.do_get(ticket)
+
+    for batch in response:
+        print(batch)
 
 
+def do_put(flight_client, table_name):
+    upload_descriptor = pyarrow.flight.FlightDescriptor.for_path(table_name)
+    writer, _ = flight_client.do_put(upload_descriptor, get_pyarrow_schema())
+
+    record_batch = create_record_batch(10000)
+    writer.write(record_batch)
+
+
+def do_action(flight_client, action_type, action_body_str):
+    action_body = str.encode(action_body_str)
+    action = pyarrow.flight.Action(action_type, action_body)
+    result = flight_client.do_action(action)
+
+    print(list(result))
+
+
+def list_actions(flight_client):
+    result = flight_client.list_actions()
+
+    print(list(result))
+
+
+# Main Function.
 if __name__ == '__main__':
     flight_client = flight.FlightClient('grpc://127.0.0.1:9999')
 
     list_actions(flight_client)
-    create_table_action(flight_client, "test_table_1")
-    create_model_table_action(flight_client, "test_model_table_1")
+    do_action(flight_client, "CommandStatementUpdate", "CREATE TABLE test_table_1(timestamp TIMESTAMP, values REAL, metadata REAL)")
+    do_action(flight_client, "CommandStatementUpdate", "CREATE MODEL TABLE test_model_table_1(location TAG, install_year TAG, model TAG, timestamp TIMESTAMP, power_output FIELD, wind_speed FIELD, temperature FIELD(5))")
 
     list_flights(flight_client)
     get_schema(flight_client, "test_table_1")
