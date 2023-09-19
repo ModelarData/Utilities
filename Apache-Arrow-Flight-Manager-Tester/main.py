399d622b535979640d5abab5e121f8e9b30cea5c
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2023-09-19 23:08:47 +0200

    Added functions to list and do actions

--- Apache-Arrow-Flight-Manager-Tester/main.py
+++ Apache-Arrow-Flight-Manager-Tester/main.py
@@ -1,4 +1,19 @@
 from pyarrow import flight
 
+
+def do_action(flight_client, action_type, action_body_str):
+    action_body = str.encode(action_body_str)
+    action = flight.Action(action_type, action_body)
+    response = flight_client.do_action(action)
+
+    print(list(response))
+
+
+def list_actions(flight_client):
+    response = flight_client.list_actions()
+
+    print(list(response))
+
+
 if __name__ == "__main__":
-    flight_client = flight.FlightClient("grpc://127.0.0.1:8888")
+    manager_client = flight.FlightClient("grpc://127.0.0.1:8888")
