af25577a402f14f699b20bf981e92b303dea1fa8
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2023-09-20 21:20:35 +0200

    Added type hints and helper function to list table names

--- Apache-Arrow-Flight-Manager-Tester/main.py
+++ Apache-Arrow-Flight-Manager-Tester/main.py
@@ -1,19 +1,37 @@
 from pyarrow import flight
+from pyarrow._flight import FlightInfo, FlightClient, ActionType
 
 
-def do_action(flight_client, action_type, action_body_str):
-    action_body = str.encode(action_body_str)
-    action = flight.Action(action_type, action_body)
+# PyArrow Functions.
+def list_flights(flight_client: FlightClient) -> list[FlightInfo]:
+    response = flight_client.list_flights()
+
+    return list(response)
+
+
+def do_action(flight_client: FlightClient, action_type: str, action_body: str) -> list[any]:
+    action_body_bytes = str.encode(action_body)
+    action = flight.Action(action_type, action_body_bytes)
     response = flight_client.do_action(action)
 
-    print(list(response))
+    return list(response)
 
 
-def list_actions(flight_client):
+def list_actions(flight_client: FlightClient) -> list[ActionType]:
     response = flight_client.list_actions()
 
-    print(list(response))
+    return list(response)
+
+
+# Helper functions.
+def list_table_names(flight_client: FlightClient) -> list[str]:
+    flights = list_flights(flight_client)
+
+    return [table_name.decode("utf-8") for table_name in flights[0].descriptor.path]
 
 
 if __name__ == "__main__":
     manager_client = flight.FlightClient("grpc://127.0.0.1:8888")
+
+    print(list_actions(manager_client))
+    print(list_table_names(manager_client))
