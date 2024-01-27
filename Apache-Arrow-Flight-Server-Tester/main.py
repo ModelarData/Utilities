96185c8a4f771de3960a717aa43e721868b84c62
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2024-01-27 18:46:22 +0100

    Added testing script for manager (#32)
    * Renamed Apache Arrow Flight server testing folder
    
    * Added structure for testing manager
    
    * Added functions to list and do actions
    
    * Added type hints and helper function to list table names
    
    * Added function to get the schema
    
    * Added util function to initialize database
    
    * Added util functions to update remote object store
    
    * Added function to update configuration
    
    * Added function to execute a query through the manager
    
    * Creating tables in manager main function
    
    * Moved helper functions down to make structure consistent
    
    * Added types to Apache Flight server tester for consistency
    
    * Updated based on comments from @skejserjensen

--- Apache-Arrow-Flight-Server-Tester/main.py
+++ Apache-Arrow-Flight-Server-Tester/main.py
@@ -5,10 +5,56 @@ from typing import Literal
 
 import pyarrow
 from pyarrow import flight
+from pyarrow._flight import FlightClient
+
+
+# PyArrow Functions.
+def list_flights(flight_client: FlightClient) -> None:
+    response = flight_client.list_flights()
+
+    print(list(response))
+
+
+def get_schema(flight_client: FlightClient, table_name: str) -> None:
+    upload_descriptor = flight.FlightDescriptor.for_path(table_name)
+    response = flight_client.get_schema(upload_descriptor)
+
+    print(response.schema)
+
+
+def do_get(flight_client: FlightClient, query: str) -> None:
+    ticket = flight.Ticket(query)
+    response = flight_client.do_get(ticket)
+
+    for batch in response:
+        pprint.pprint(batch.data.to_pydict())
+
+
+def do_put(flight_client: FlightClient, table_name: str, num_rows: int) -> None:
+    upload_descriptor = flight.FlightDescriptor.for_path(table_name)
+    writer, _ = flight_client.do_put(upload_descriptor, get_pyarrow_schema())
+
+    record_batch = create_record_batch(num_rows)
+    writer.write(record_batch)
+    writer.close()
+
+
+def do_action(flight_client: FlightClient, action_type: str, action_body: str) -> None:
+    action_body_bytes = str.encode(action_body)
+    action = flight.Action(action_type, action_body_bytes)
+    response = flight_client.do_action(action)
+
+    print(list(response))
+
+
+def list_actions(flight_client: FlightClient) -> None:
+    response = flight_client.list_actions()
+
+    print(list(response))
 
 
 # Helper Functions.
-def get_pyarrow_schema():
+def get_pyarrow_schema() -> pyarrow.Schema:
     return pyarrow.schema(
         [
             ("location", pyarrow.utf8()),
@@ -22,7 +68,7 @@ def get_pyarrow_schema():
     )
 
 
-def create_record_batch(num_rows):
+def create_record_batch(num_rows: int) -> pyarrow.RecordBatch:
     location = ["aalborg" if i % 2 == 0 else "nibe" for i in range(num_rows)]
     install_year = ["2021" if i % 2 == 0 else "2022" for i in range(num_rows)]
     model = ["w72" if i % 2 == 0 else "w73" for i in range(num_rows)]
@@ -46,47 +92,16 @@ def create_record_batch(num_rows):
     )
 
 
-# PyArrow Functions.
-def list_flights(flight_client):
-    response = flight_client.list_flights()
+def update_configuration(flight_client: flight.FlightClient, setting: str, setting_value: str) -> None:
+    setting_bytes = str.encode(setting)
+    setting_size = len(setting_bytes).to_bytes(2, byteorder="big")
 
-    print(list(response))
+    setting_value_bytes = str.encode(setting_value)
+    setting_value_size = len(setting_value_bytes).to_bytes(2, byteorder="big")
 
-
-def get_schema(flight_client, table_name):
-    upload_descriptor = pyarrow.flight.FlightDescriptor.for_path(table_name)
-    response = flight_client.get_schema(upload_descriptor)
-
-    print(response.schema)
-
-
-def do_get(flight_client, ticket):
-    ticket = flight.Ticket(ticket)
-    response = flight_client.do_get(ticket)
-
-    for batch in response:
-        pprint.pprint(batch.data.to_pydict())
-
-
-def do_put(flight_client, table_name):
-    upload_descriptor = pyarrow.flight.FlightDescriptor.for_path(table_name)
-    writer, _ = flight_client.do_put(upload_descriptor, get_pyarrow_schema())
-
-    record_batch = create_record_batch(10000)
-    writer.write(record_batch)
-    writer.close()
-
-
-def do_action(flight_client, action_type, action_body_str):
-    action_body = str.encode(action_body_str)
-    action = pyarrow.flight.Action(action_type, action_body)
+    action_body = setting_size + setting_bytes + setting_value_size + setting_value_bytes
+    action = flight.Action("UpdateConfiguration", action_body)
     response = flight_client.do_action(action)
-
-    print(list(response))
-
-
-def list_actions(flight_client):
-    response = flight_client.list_actions()
 
     print(list(response))
 
@@ -101,7 +116,7 @@ def update_object_store(flight_client: flight.FlightClient, object_store_type: L
     arguments.insert(0, object_store_type)
     action_body = create_update_object_store_action_body(arguments)
 
-    result = flight_client.do_action(pyarrow.flight.Action("UpdateRemoteObjectStore", action_body))
+    result = flight_client.do_action(flight.Action("UpdateRemoteObjectStore", action_body))
 
     print(list(result))
 
@@ -123,6 +138,7 @@ if __name__ == "__main__":
     flight_client = flight.FlightClient("grpc://127.0.0.1:9999")
 
     list_actions(flight_client)
+
     do_action(
         flight_client,
         "CommandStatementUpdate",
@@ -140,5 +156,5 @@ if __name__ == "__main__":
     get_schema(flight_client, "test_table_1")
     get_schema(flight_client, "test_model_table_1")
 
-    do_put(flight_client, "test_model_table_1")
+    do_put(flight_client, "test_model_table_1", 10000)
     do_get(flight_client, "SELECT * FROM test_model_table_1 LIMIT 5")
