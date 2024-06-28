f583b8354c11359e3fc286ea7c4f38eb2e414556
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2024-06-28 13:41:51 +0200

    Moved unique server helper functions to shared file

--- Apache-Arrow-Flight-Tester/main.py
+++ Apache-Arrow-Flight-Tester/main.py
@@ -1,8 +1,11 @@
 import pprint
+import time
+from random import randrange
 from typing import Literal
 
+import pyarrow
 from pyarrow import flight, Schema
-from pyarrow._flight import FlightInfo, FlightClient, ActionType, Result
+from pyarrow._flight import FlightInfo, FlightClient, ActionType, Result, Ticket
 
 
 # PyArrow Functions.
@@ -17,6 +20,22 @@ def get_schema(flight_client: FlightClient, table_name: str) -> Schema:
     response = flight_client.get_schema(upload_descriptor)
 
     return response.schema
+
+
+def do_get(flight_client: FlightClient, ticket: Ticket) -> None:
+    response = flight_client.do_get(ticket)
+
+    for batch in response:
+        pprint.pprint(batch.data.to_pydict())
+
+
+def do_put(flight_client: FlightClient, table_name: str, num_rows: int) -> None:
+    upload_descriptor = flight.FlightDescriptor.for_path(table_name)
+    writer, _ = flight_client.do_put(upload_descriptor, get_pyarrow_schema())
+
+    record_batch = create_record_batch(num_rows)
+    writer.write(record_batch)
+    writer.close()
 
 
 def do_action(flight_client: FlightClient, action_type: str, action_body: str) -> list[Result]:
@@ -34,6 +53,44 @@ def list_actions(flight_client: FlightClient) -> list[ActionType]:
 
 
 # Helper functions.
+def get_pyarrow_schema() -> pyarrow.Schema:
+    return pyarrow.schema(
+        [
+            ("location", pyarrow.utf8()),
+            ("install_year", pyarrow.utf8()),
+            ("model", pyarrow.utf8()),
+            ("timestamp", pyarrow.timestamp("ms")),
+            ("power_output", pyarrow.float32()),
+            ("wind_speed", pyarrow.float32()),
+            ("temperature", pyarrow.float32()),
+        ]
+    )
+
+
+def create_record_batch(num_rows: int) -> pyarrow.RecordBatch:
+    location = ["aalborg" if i % 2 == 0 else "nibe" for i in range(num_rows)]
+    install_year = ["2021" if i % 2 == 0 else "2022" for i in range(num_rows)]
+    model = ["w72" if i % 2 == 0 else "w73" for i in range(num_rows)]
+
+    timestamp = [round(time.time() * 1000) + (i * 1000) for i in range(num_rows)]
+    power_output = [float(randrange(0, 30)) for _ in range(num_rows)]
+    wind_speed = [float(randrange(50, 100)) for _ in range(num_rows)]
+    temperature = [float(randrange(0, 40)) for _ in range(num_rows)]
+
+    return pyarrow.RecordBatch.from_arrays(
+        [
+            location,
+            install_year,
+            model,
+            timestamp,
+            power_output,
+            wind_speed,
+            temperature,
+        ],
+        schema=get_pyarrow_schema(),
+    )
+
+
 def list_table_names(flight_client: FlightClient) -> list[str]:
     flights = list_flights(flight_client)
 
@@ -47,7 +104,7 @@ def initialize_database(flight_client: FlightClient, tables: list[str]) -> list[
     return decoded_result.split(";")
 
 
-def execute_query(flight_client: FlightClient, query: str) -> None:
+def execute_query_with_manager(flight_client: FlightClient, query: str) -> None:
     # Retrieve the flight info that describes how to execute the query.
     query_descriptor = flight.FlightDescriptor.for_command(query)
     flight_info = flight_client.get_flight_info(query_descriptor)
@@ -57,10 +114,21 @@ def execute_query(flight_client: FlightClient, query: str) -> None:
     cloud_node_url = endpoint.locations[0]
 
     cloud_client = flight.FlightClient(cloud_node_url)
-    response = cloud_client.do_get(endpoint.ticket)
+    do_get(cloud_client, endpoint.ticket)
 
-    for batch in response:
-        pprint.pprint(batch.data.to_pydict())
+
+def update_configuration(flight_client: flight.FlightClient, setting: str, setting_value: str) -> list[Result]:
+    setting_bytes = str.encode(setting)
+    setting_size = len(setting_bytes).to_bytes(2, byteorder="big")
+
+    setting_value_bytes = str.encode(setting_value)
+    setting_value_size = len(setting_value_bytes).to_bytes(2, byteorder="big")
+
+    action_body = setting_size + setting_bytes + setting_value_size + setting_value_bytes
+    action = flight.Action("UpdateConfiguration", action_body)
+    response = flight_client.do_action(action)
+
+    return list(response)
 
 
 def update_object_store(flight_client: flight.FlightClient, object_store_type: Literal["s3", "azureblobstorage"],
