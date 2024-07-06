e2ef0c2eb86cee21e2bf117d83c55d59ffcae86b
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2024-07-06 18:23:21 +0200

    Added structure for retrieving a GitHub PAT from config file

--- Library-Tester/main.py
+++ Library-Tester/main.py
@@ -1,4 +1,10 @@
 """Script for testing the ModelarDB Python library."""
 
+import configparser
+
 if __name__ == "__main__":
-    print("Hello, World!")
+    config = configparser.ConfigParser()
+    config.read("config.ini")
+
+    personal_access_token = config["DEFAULT"]["GITHUB_PERSONAL_ACCESS_TOKEN"]
+    print(personal_access_token)
