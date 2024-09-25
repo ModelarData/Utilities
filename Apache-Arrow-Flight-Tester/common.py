ffa1b0e2bdeed98c57747eb163977f27810766ac
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2024-09-25 21:35:41 +0200

    Added a class for the common functionality

--- Apache-Arrow-Flight-Tester/common.py
+++ Apache-Arrow-Flight-Tester/common.py
@@ -1,57 +1,75 @@
-import pprint
-
-import pyarrow
 from pyarrow import flight, Schema
-from pyarrow._flight import FlightInfo, FlightClient, ActionType, Result, Ticket
+from pyarrow._flight import FlightInfo, ActionType, Result
 
 
-# Apache Arrow Flight functions.
-def list_flights(flight_client: FlightClient) -> list[FlightInfo]:
-    response = flight_client.list_flights()
+class ModelarDBFlightClient:
+    """Common functionality for interacting with server and manager ModelarDB instances using Apache Arrow Flight."""
 
-    return list(response)
+    def __init__(self, location: str):
+        self.flight_client = flight.FlightClient(location)
 
+    def list_flights(self) -> list[FlightInfo]:
+        """Wrapper around the list_flights method of the FlightClient class."""
+        response = self.flight_client.list_flights()
 
-def get_schema(flight_client: FlightClient, table_name: str) -> Schema:
-    upload_descriptor = flight.FlightDescriptor.for_path(table_name)
-    response = flight_client.get_schema(upload_descriptor)
+        return list(response)
 
-    return response.schema
+    def get_schema(self, table_name: str) -> Schema:
+        """Wrapper around the get_schema method of the FlightClient class."""
+        upload_descriptor = flight.FlightDescriptor.for_path(table_name)
+        response = self.flight_client.get_schema(upload_descriptor)
 
+        return response.schema
 
-def do_get(flight_client: FlightClient, ticket: Ticket) -> None:
-    response = flight_client.do_get(ticket)
+    def do_action(self, action_type: str, action_body: bytes) -> list[Result]:
+        """Wrapper around the do_action method of the FlightClient class."""
+        action = flight.Action(action_type, action_body)
+        response = self.flight_client.do_action(action)
 
-    for batch in response:
-        pprint.pprint(batch.data.to_pydict())
+        return list(response)
 
+    def list_actions(self) -> list[ActionType]:
+        """Wrapper around the list_actions method of the FlightClient class."""
+        response = self.flight_client.list_actions()
 
-def do_put(flight_client: FlightClient, table_name: str, record_batch: pyarrow.RecordBatch) -> None:
-    upload_descriptor = flight.FlightDescriptor.for_path(table_name)
-    writer, _ = flight_client.do_put(upload_descriptor, record_batch.schema)
+        return list(response)
 
-    writer.write(record_batch)
-    writer.close()
+    def list_table_names(self) -> list[str]:
+        """Return the names of the tables in the server or manager."""
+        flights = self.list_flights()
+        return [table_name.decode("utf-8") for table_name in flights[0].descriptor.path]
 
+    def create_table(self, table_name: str, columns: list[tuple[str, str]]) -> None:
+        """
+        Create a table in the server or manager with the given name and columns. Each pair in columns should have the
+        format (column_name, column_type).
+        """
+        sql = f"CREATE TABLE {table_name}({', '.join([f'{column[0]} {column[1]}' for column in columns])})"
+        self.do_action("CommandStatementUpdate", str.encode(sql))
 
-def do_action(flight_client: FlightClient, action_type: str, action_body: bytes) -> list[Result]:
-    action = flight.Action(action_type, action_body)
-    response = flight_client.do_action(action)
+    def create_test_tables(self) -> None:
+        """
+        Create a table and a model table using the flight client, print the current tables to ensure the created tables are
+        included, and print the schema for the created table and model table to ensure the tables are created correctly.
+        """
+        print("Creating test tables...")
 
-    return list(response)
+        self.create_table("test_table_1", [("timestamp", "TIMESTAMP"), ("values", "REAL"), ("metadata", "REAL")])
+        self.create_table("test_model_table_1", [
+            ("location", "TAG"), ("install_year", "TAG"), ("model", "TAG"),
+            ("timestamp", "TIMESTAMP"), ("power_output", "FIELD"),
+            ("wind_speed", "FIELD"), ("temperature", "FIELD(5%)")
+        ])
 
+        print("\nCurrent tables:")
+        table_names = self.list_table_names()
+        print(table_names)
 
-def list_actions(flight_client: FlightClient) -> list[ActionType]:
-    response = flight_client.list_actions()
+        print(f"\nSchema for {table_names[0]}:")
+        print(self.get_schema(table_names[0]))
 
-    return list(response)
-
-
-# Helper functions.
-def list_table_names(flight_client: FlightClient) -> list[str]:
-    flights = list_flights(flight_client)
-
-    return [table_name.decode("utf-8") for table_name in flights[0].descriptor.path]
+        print(f"\nSchema for {table_names[1]}:")
+        print(self.get_schema(table_names[1]))
 
 
 def encode_argument(argument: str) -> bytes:
@@ -59,33 +77,3 @@ def encode_argument(argument: str) -> bytes:
     argument_size = len(argument_bytes).to_bytes(2, byteorder="big")
 
     return argument_size + argument_bytes
-
-
-def create_test_tables(flight_client: FlightClient) -> None:
-    """
-    Create a table and a model table using the flight client, print the current tables to ensure the created tables are
-    included, and print the schema for the created table and model table to ensure the tables are created correctly.
-    """
-    print("Creating test tables...")
-    print(do_action(
-        flight_client,
-        "CommandStatementUpdate",
-        str.encode("CREATE TABLE test_table_1(timestamp TIMESTAMP, values REAL, metadata REAL)"),
-    ))
-    print(do_action(
-        flight_client,
-        "CommandStatementUpdate",
-        str.encode("CREATE MODEL TABLE test_model_table_1(location TAG, install_year TAG, model "
-                   "TAG, timestamp TIMESTAMP, power_output FIELD, wind_speed FIELD, temperature "
-                   "FIELD(5%))"),
-    ))
-
-    print("\nCurrent tables:")
-    table_names = list_table_names(flight_client)
-    print(table_names)
-
-    print(f"\nSchema for {table_names[0]}:")
-    print(get_schema(flight_client, table_names[0]))
-
-    print(f"\nSchema for {table_names[1]}:")
-    print(get_schema(flight_client, table_names[1]))
