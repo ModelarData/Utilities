bdd0ede067de84e2e9496589a7beb9dc71aaefb1
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2024-09-28 13:33:54 +0200

    New design for Apache Arrow Flight testers (#41)
    * Added a class for the common functionality
    
    * Added a method to drop tables in common class
    
    * Created a server class for testing the server
    
    * Created a manager class for testing the manager
    
    * Fixed bug causing create_table() to only create normal tables
    
    * Removed method to drop table for now
    
    * Updated based on comments from @skejserjensen

--- Apache-Arrow-Flight-Tester/manager.py
+++ Apache-Arrow-Flight-Tester/manager.py
@@ -1,50 +1,65 @@
-import common
-
 from typing import Literal
 
 from pyarrow import flight
-from pyarrow._flight import FlightClient, Result
+from pyarrow._flight import Result
+
+from common import ModelarDBFlightClient, encode_argument
+from server import ModelarDBServerFlightClient
 
 
-# Helper functions.
-def initialize_database(flight_client: FlightClient, tables: list[str]) -> list[str]:
-    result = common.do_action(flight_client, "InitializeDatabase", str.encode(",".join(tables)))[0]
-    decoded_result = result.body.to_pybytes().decode("utf-8")
+class ModelarDBManagerFlightClient(ModelarDBFlightClient):
+    """Functionality for interacting with a ModelarDB manager using Apache Arrow Flight."""
 
-    return decoded_result.split(";")
+    def initialize_database(self, existing_tables: list[str]) -> list[str]:
+        """
+        Retrieve the SQL statements required to initialize the database with the tables that are not included in the
+        given list of tables. Throws an error if a table in the given list does not exist in the database.
+        """
+        result = self.do_action("InitializeDatabase", str.encode(",".join(existing_tables)))[0]
+        decoded_result = result.body.to_pybytes().decode("utf-8")
 
+        return decoded_result.split(";")
 
-def register_node(flight_client: FlightClient, node_url: str, node_mode: Literal["cloud", "edge"]) -> list[Result]:
-    encoded_node_url = common.encode_argument(node_url)
-    encoded_node_mode = common.encode_argument(node_mode)
+    def register_node(self, node_url: str, node_mode: Literal["cloud", "edge"]) -> list[Result]:
+        """Register a node with the given URL and mode in the manager."""
+        encoded_node_url = encode_argument(node_url)
+        encoded_node_mode = encode_argument(node_mode)
 
-    action_body = encoded_node_url + encoded_node_mode
-    response = common.do_action(flight_client, "RegisterNode", action_body)
+        action_body = encoded_node_url + encoded_node_mode
+        response = self.do_action("RegisterNode", action_body)
 
-    return response[0].body.to_pybytes()
+        return response[0].body.to_pybytes()
 
+    def remove_node(self, node_url: str) -> list[Result]:
+        """Remove the node with the given URL from the manager."""
+        encoded_node_url = encode_argument(node_url)
 
-def remove_node(flight_client: FlightClient, node_url: str) -> list[Result]:
-    encoded_node_url = common.encode_argument(node_url)
+        return self.do_action("RemoveNode", encoded_node_url)
 
-    return common.do_action(flight_client, "RemoveNode", encoded_node_url)
+    def query(self, query: str) -> None:
+        """
+        Retrieve a cloud node that can execute the given query and execute the query on the node. It is assumed that
+        at least one cloud node is already registered with the manager.
+        """
+        print("Retrieving cloud node that can execute the query...")
+        query_descriptor = flight.FlightDescriptor.for_command(query)
+        flight_info = self.flight_client.get_flight_info(query_descriptor)
 
+        endpoint = flight_info.endpoints[0]
+        cloud_node_url = endpoint.locations[0]
 
-def execute_query(flight_client: FlightClient, query: str) -> None:
-    print("Retrieving cloud node that can execute the query...")
-    query_descriptor = flight.FlightDescriptor.for_command(query)
-    flight_info = flight_client.get_flight_info(query_descriptor)
-
-    endpoint = flight_info.endpoints[0]
-    cloud_node_url = endpoint.locations[0]
-
-    print(f"Executing query on {cloud_node_url}...")
-    cloud_client = flight.FlightClient(cloud_node_url)
-    common.do_get(cloud_client, endpoint.ticket)
+        print(f"Executing query on {cloud_node_url}...")
+        cloud_client = ModelarDBServerFlightClient(cloud_node_url)
+        cloud_client.do_get(endpoint.ticket)
 
 
 if __name__ == "__main__":
-    manager_client = flight.FlightClient("grpc://127.0.0.1:9998")
-    common.create_test_tables(manager_client)
+    manager_client = ModelarDBManagerFlightClient("grpc://127.0.0.1:9998")
+    manager_client.create_test_tables()
 
-    execute_query(manager_client, "SELECT * FROM test_model_table_1 LIMIT 5")
+    print(manager_client.initialize_database(["test_table_1"]))
+
+    print(manager_client.register_node("grpc://127.0.0.1:9999", "edge"))
+    print(manager_client.remove_node("grpc://127.0.0.1:9999"))
+
+    manager_client.query("SELECT * FROM test_model_table_1 LIMIT 5")
