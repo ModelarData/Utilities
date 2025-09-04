b4b6dc38d75e5d7b8199aff21edb21561c990df7
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2025-09-04 10:12:42 +0200

    Add method to create test tables using CreateTable action

--- Apache-Arrow-Flight-Tester/common.py
+++ Apache-Arrow-Flight-Tester/common.py
@@ -65,9 +65,8 @@ class ModelarDBFlightClient:
 
     def create_test_tables(self) -> None:
         """
-        Create a table and a time series table using the flight client, print the current tables to ensure the created
-        tables are included, and print the schema for the created table and time series table to ensure the tables are
-        created correctly.
+        Create a normal table and a time series table using the flight client, print the current tables to ensure the
+        created tables are included, and print the schema to ensure the tables are created correctly.
         """
         print("Creating test tables...")
 
@@ -120,6 +119,38 @@ class ModelarDBFlightClient:
         table_metadata.time_series_table.CopyFrom(time_series_table_metadata)
 
         self.do_action("CreateTable", table_metadata.SerializeToString())
+
+    def create_test_tables_from_metadata(self):
+        """
+        Create a normal table and a time series table using the CreateTable action, print the current tables to ensure
+        the created tables are included, and print the schema to ensure the tables are created correctly.
+        """
+        print("Creating test tables from metadata...")
+
+        normal_table_schema = pyarrow.schema([
+            ("timestamp", pyarrow.timestamp("us")),
+            ("values", pyarrow.float32()),
+            ("metadata", pyarrow.utf8())
+        ])
+
+        self.create_normal_table_from_metadata("test_table_1", normal_table_schema)
+
+        time_series_table_schema = get_time_series_table_schema()
+
+        absolute = protocol_pb2.TableMetadata.TimeSeriesTableMetadata.ErrorBound.Type.ABSOLUTE
+        error_bounds = [protocol_pb2.TableMetadata.TimeSeriesTableMetadata.ErrorBound(value=0, type=absolute)
+                        for _ in range(len(time_series_table_schema))]
+
+        generated_column_expressions = [b'' for _ in range(len(time_series_table_schema))]
+
+        self.create_time_series_table_from_metadata("test_time_series_table_1", time_series_table_schema,
+                                                    error_bounds, generated_column_expressions)
+
+        print("\nCurrent tables:")
+        for table_name in self.list_table_names():
+            print(f"{table_name}:")
+            print(f"{self.get_schema(table_name)}\n")
+
     def drop_table(self, table_name: str) -> None:
         """Drop the table with the given name from the server or manager."""
         self.do_get(Ticket(f"DROP TABLE {table_name}"))
