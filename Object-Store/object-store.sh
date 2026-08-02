eacd86952161ae89ce21db7baef4ef75f8b8bd65
Søren Kejser Jensen <devel@kejserjensen.dk>
2026-08-02 18:23:37 +0200

    Update based on comments from @Copilot

--- Object-Store/object-store.sh
+++ Object-Store/object-store.sh
@@ -1,4 +1,5 @@
-#!/usr/bin/env {bash,zsh}
+# This Bash/ZSH script simplify running tests that use Azurite and/or Minio.
+# It purposely does not have a #! as it is written to work with Bash and ZSH.
 set -e
 
 
@@ -13,21 +14,25 @@ AZURITE_PID=
 # API: http://127.0.0.1:9000
 # WebUI: http://127.0.0.1:????? # Port is randomized.
 start_minio() {
+    print_arrow_message "Start Minio"
     MINIO_FOLDER=$(mktemp -d)
     minio server "$MINIO_FOLDER" &
     MINIO_PID=$!
     sleep 1 # Simple way to ensure Minio has time to start.
     mcli alias set local http://127.0.0.1:9000 minioadmin minioadmin
     mcli mb local/modelardb
+    echo
 }
 
 stop_minio() {
+    print_arrow_message "Stop Minio"
     if [ ! -z "$MINIO_FOLDER" ]
     then
 	kill "$MINIO_PID"
 	sleep 1 # Simple way to ensure Minio has time to write.
 	rm -rf "$MINIO_FOLDER"
     fi
+    echo
 }
 
 # Azurite Blob service is starting at http://127.0.0.1:10000
@@ -37,35 +42,32 @@ stop_minio() {
 # Azurite Table service is starting at http://127.0.0.1:10002
 # Azurite Table service is successfully listening at http://127.0.0.1:10002
 start_azurite() {
+    print_arrow_message "Start Azurite"
     AZURITE_FOLDER=$(mktemp -d)
     azurite -l "$AZURITE_FOLDER" --silent --skipApiVersionCheck &
     AZURITE_PID=$!
     sleep 1 # Simple way to ensure Azurite has time to start.
     az storage container create -n modelardb --connection-string 'DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;'
+    echo
 }
 
 stop_azurite() {
+    print_arrow_message "Stop Azurite"
     if [ ! -z "$AZURITE_FOLDER" ]
     then
 	kill "$AZURITE_PID"
 	sleep 1 # Simple way to ensure Azurite has time to write.
 	rm -rf "$AZURITE_FOLDER"
     fi
+    echo
 }
 
 print_error_message() {
-    printf "\033[1;31mERROR: \033[39m%s\n" $1
+    printf "\033[1;31mERROR: \033[39m%s\n" "$1"
 }
 
 print_arrow_message() {
-  printf "\033[1;92m==> \033[39m%s \033[m\n" $1
-}
-
-print_arrow_message_and_run() {
-    print_arrow_message "$1"
-    "$1"
-    sleep 1
-    echo
+  printf "\033[1;92m==> \033[39m%s \033[m\n" "$1"
 }
 
 check_for_dependencies() {
@@ -87,9 +89,9 @@ check_for_dependencies
 
 trap 'stop_minio; stop_azurite' EXIT
 
-print_arrow_message_and_run "start_minio"
-print_arrow_message_and_run "start_azurite"
+start_minio
+start_azurite
 
-print_arrow_message "waiting"
-read -p "Started object stores, press enter to stop."
-echo
+print_arrow_message "Waiting"
+echo "Started object stores, press enter to stop."
+read
